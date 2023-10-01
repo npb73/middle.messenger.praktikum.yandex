0adf696a536a58ea47e495d5c2967f7348698256
npb73 <nvo.is171@gmail.com>
2023-10-02 00:42:55 +0700

    inputs style

--- src/components/CustomInput/index.tmpl.js
+++ src/components/CustomInput/index.tmpl.js
@@ -2,5 +2,9 @@ import * as style from './style.module.sass'
 
 export const tmpl = 
 `
-<input class="${style.customInput}" type="text" placeholder="{{placeholder}}"/>
+<div class="${style.inputWrapper}">
+<span class="${style.inputName}">{{placeholder}}</span>
+<input name="{{name}}" class="${style.customInput}" type="text" placeholder="{{placeholder}}"/>
+</div>
+
 `
--- src/pages/LoginPage/index.js
+++ src/pages/LoginPage/index.js
@@ -5,9 +5,9 @@ import CustomInput from "../../components/CustomInput"
 
 const LoginPage = () => {
   return Handlebars.compile(tmpl)({
-    login: CustomInput({placeholder: 'login'}),
-    password: CustomInput({placeholder: 'password'})
-  });
+    login: CustomInput({placeholder: 'login', name: 'login', type: 'text'}),
+    password: CustomInput({placeholder: 'password', name: 'password', type: 'password'})
+  })
 }
 
 export default LoginPage;

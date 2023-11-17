30bee40ac8aee1a04ddff1c211d56251628576d4
npb73 <nvo.is171@gmail.com>
2023-11-18 05:32:40 +0700

    aaaaaaaaaaaaaaaaa

--- src/components/ProfileField/profileField.tmpl.ts
+++ src/components/ProfileField/profileField.tmpl.ts
@@ -1,5 +1,5 @@
 export const tmpl = `
-<div class='profileField_container'>
+<div class='profileFieldContainer'>
     <p class='fieldName'>{{fieldName}}</p>
     <p class='fieldValue'>{{fieldValue}}</p>
 </div>`;
